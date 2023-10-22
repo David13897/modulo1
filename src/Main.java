d34d0d97c04bc593d9835d17c136785eacb2a6bc
david <dsng2002@gmail.com>
2023-10-22 18:48:29 +0100

    va modulo1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,80 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[]args){
+        //va01
         System.out.println("+-----+");
         System.out.println("|david|");
         System.out.println("+-----+");
+
+        //va02
+        String nome = "fortaleza";
+        int len = nome.length();
+        System.out.println(len);
+
+        String nome2 = "joana";
+        String n = nome2.toUpperCase();
+        System.out.println(n);
+
+        String nome3 = "ana";
+        //nome3.println();
+        //nao esta correto.
+
+        //va03
+        double[] data = new double[10];
+        for (int i=0; i < data.length; i++)
+            data[i]=i*i;
+        System.out.println(data);
+
+        //va05
+        //a)
+        for (int a=100; a<201; a++)
+            System.out.println(a);
+
+        //b)
+        int soma=0;
+        for (int b=2; b<1000; b+=2)
+            soma += b;
+        System.out.println(soma);
+
+        //c)
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print("\n");
+        //e)
+        int primeiroTermo = 0;
+        int segundoTermo = 1;
+
+        while (primeiroTermo <= 50) {
+            System.out.print(primeiroTermo + " ");
+
+            int proximoTermo = primeiroTermo + segundoTermo;
+            primeiroTermo = segundoTermo;
+            segundoTermo = proximoTermo;
+        }
+        //f)
+        System.out.print("\n");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor de x: ");
+        int x = scanner.nextInt();
+
+        while (x != 1) {
+            System.out.print(x + " -> ");
+
+            if (x % 2 == 0) {
+                x = x / 2;
+            } else {
+                x = 3 * x + 1;
+            }
+        }
+
+        // Imprime o último valor (1) após o loop
+        System.out.println(x);
+
+        // Fecha o scanner
+        scanner.close();
     }
 }
